b38fb54bc53889fd497d47de02cd70eb1ffe9b92
mtac002 <mtac002@outlook.com>
2014-11-04 01:44:08 -0500

    First Commit To this project!

---
+++ java-cp.bat
@@ -0,0 +1,9 @@
+java -cp target/SKCraftLauncher.jar com.skcraft.launcher.builder.PackageBuilder
+  --version "5.1.2"
+  --manifest-dest "dist\manifest.json"
+  --objects-dest "dist\objects"
+  --files "src"
+  --config "modpack.json"
+  --objects-url "http://hostilethon.com/downloads/objects"
+  --version-file "version.json"
+  --libs-url "http://hostilethon.com/downloads/libraries"
